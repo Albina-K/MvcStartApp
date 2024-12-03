df971ccf864128f2acb614f1921347cd5915cae9
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-12-03 15:32:30 +0300

    процесс

---
+++ MvcStartApp/Models/Db/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Models.Db
+{
+    public class BlogRepository : IBlogRepository
+    {
+        //ссылка на контекст
+        private readonly BlogContext _context;
+
+        //метод-конструктор для инициализации
+        public BlogRepository(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUser(User user)
+        {
+            //добавление пользователя
+            var entry = _context.Entry(user);
+            if (entry.State == EntityState.Detached)
+                await _context.Users.AddAsync(user);
+
+            //сщхранение ихменений
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- MvcStartApp/Startup.cs
+++ MvcStartApp/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MvcStartApp.Middleware;
+using MvcStartApp.Models.Db;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace MvcStartApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BlogContext>(options => options.UseSqlServer(connection));
+            services.AddScoped<IBlogRepository, BlogRepository>();
             services.AddControllersWithViews();
         }
 
